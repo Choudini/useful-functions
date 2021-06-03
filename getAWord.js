979ecf005537b0bc4d5ec3952b93039f40dde920
Praumat <40520430+PraumatFR@users.noreply.github.com>
2021-06-03 19:00:19 +0200

    Create getAWord.js

---
+++ getAWord.js
@@ -0,0 +1,17 @@
+
+
+// More explanation in the Wiki page about why you might need this.
+function getAWord(wordId, message) {
+      // this function update the lang 
+    updateLang()
+      // exclude the first word, in my case, the first word is the trigger.
+    var args = message.content.trim().split(/ +/g)
+      // just checking if you typed something after the trigger
+    if (args.length > 1) {
+      // Here for example, if wordId = 1, then isolate the first word after the trigger (the second word) 
+        return args.splice(wordId, wordId).shift();
+    } else {
+      // reply is a json file containing some sentences.
+        return reply.SYNTAX_ERROR
+    }
+}
