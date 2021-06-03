8a310e2d68cdcad398f21869d4198f0e4d9892ce
Praumat <40520430+PraumatFR@users.noreply.github.com>
2021-06-03 19:21:40 +0200

    Update updateLang.js

--- functions/updateLang.js
+++ functions/updateLang.js
@@ -1,1 +1,14 @@
 
+/*
+This function is not that useful, it just refresh the variable "reply" whenever I call it. You don't realy need this if you are ready to restart your program to 
+apply a language change.
+Or you can just create a manual command ¯\_(ツ)_/¯
+*/
+
+var reply = require(`./json/reply/en.json`)
+function updateLang(){
+    // Config is where I put the configuration of the bot
+    Config.findOne({ details: 'test' }, async (err, config) => {
+         reply = require(`./json/reply/${config.lang}.json`)
+    })
+}
