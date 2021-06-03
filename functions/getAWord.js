bd9144cdef062daf74903c7073f1b828785b943d
Praumat <40520430+PraumatFR@users.noreply.github.com>
2021-06-04 00:02:28 +0200

    Update 2106032355
    • New parameter : ignore (if true then return undefined when args.length = 0 || 1)
    • If args.length <= 1 it now throw an error, not a string only.

--- functions/getAWord.js
+++ functions/getAWord.js
@@ -2,17 +2,21 @@
 This function is quite useful, I use this everytime, maybe there's better way but, if I want my command to have multiple options I use this so I can read
 each argument and do things.
 */
-function getAWord(wordId, message) {
-      // this function update the language 
+function getAWord(wordId, message, ignore) {
+    // this function update the language 
     updateLang()
-      // exclude the first word on 'message', in my case, the first word is the trigger.
+    // exclude the first word on 'message', in my case, the first word is the trigger.
     var args = message.content.trim().split(/ +/g)
-      // just checking if you typed something after the trigger
+    // just checking if you typed something after the trigger
     if (args.length > 1) {
-      // Here for example, if wordId = 1, then isolate the first word after the trigger (the second word) 
+        // Here for example, if wordId = 1, then isolate the first word after the trigger (the second word) 
         return args.splice(wordId, wordId).shift();
     } else {
-      // reply is a json file containing some sentences.
-        return reply.SYNTAX_ERROR
+        // reply is a json file containing some sentences.
+        // the 'ignore' parameter is there if you want 'getAWord()' function to return nothing (undefined) when args length is equal to 1 or 0
+        if (ignore != true) {
+            throw message.channel.send(reply.SYNTAX_ERROR)
+        }
     }
 }
+
